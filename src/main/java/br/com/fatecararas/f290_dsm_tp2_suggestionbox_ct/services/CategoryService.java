3a6f79aa24a14ad23085fdd7f6b8b7296f84f925
Esdras B Silva <bzr4@icloud.com>
2023-05-19 17:36:49 -0300

    master
    - Initial commit.

---
+++ src/main/java/br/com/fatecararas/f290_dsm_tp2_suggestionbox_ct/services/CategoryService.java
@@ -0,0 +1,20 @@
+package br.com.fatecararas.f290_dsm_tp2_suggestionbox_ct.services;
+
+import org.springframework.stereotype.Service;
+
+import br.com.fatecararas.f290_dsm_tp2_suggestionbox_ct.model.entities.Category;
+import br.com.fatecararas.f290_dsm_tp2_suggestionbox_ct.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+    
+    private final CategoryRepository repository;
+
+    public CategoryService(CategoryRepository repository) {
+        this.repository = repository;
+    }
+
+    public void salvar(Category category) {
+        repository.save(category);
+    }
+}
