5b8733af75dd6f5067c91403c6e0830688f65765
esdrasilvavr <71395581+esdrasilvavr@users.noreply.github.com>
2023-05-27 14:58:49 -0300

    Atualização atividade

---
+++ src/main/java/br/com/fatecararas/f290_dsm_tp2_suggestionbox_ct/api/v1/resources/SuggestionResource.java
@@ -0,0 +1,7 @@
+package br.com.fatecararas.f290_dsm_tp2_suggestionbox_ct.api.v1.resources;
+
+
+//TODO: Configurar RestController para o recurso /suggestion
+public class SuggestionResource {
+    
+}
--- src/main/java/br/com/fatecararas/f290_dsm_tp2_suggestionbox_ct/services/SuggestionService.java
+++ src/main/java/br/com/fatecararas/f290_dsm_tp2_suggestionbox_ct/services/SuggestionService.java
@@ -5,11 +5,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class SuggestionService {
 
-    //TODO: Criar um script que crie 4 tipos de categoria [Sugestão, Crítica, Elogio, Comentário] e crie uma menos 1 registro com cbase numa categoria.
+    //TODO: Criar um script que crie 4 tipos de categoria [Sugestão, Crítica, Elogio, Comentário] e crie uma menos 1 registro com base numa categoria.
 
     //TODO: Criar método que retorne todas as sugestões do banco de dados.
 
     //TODO: Criar método que retorne uma sugestão com base num id.
 
     //TODO: Criar método que remova uma sugestão do banco de dados.
+
+    //TODO: Alterar o application-test.properties para não criar as entidades Category e Suggestion
 }
